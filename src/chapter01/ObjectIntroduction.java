07dc833b2deed720632fbce7bb3288035cbccc99
wucy0 <wucy0127@qq.com>
2023-05-23 21:38:29 +0800

    create

---
+++ src/chapter01/ObjectIntroduction.java
@@ -0,0 +1,13 @@
+package chapter01;
+
+/**
+ * @author wucy0
+ * @since 2023/5/23
+ * @deprecated  对象导论
+ */
+public class ObjectIntroduction {
+
+
+    public static void main(String[] args) {
+    }
+}
